c71413ae081cef5a3215bea8703fad8faf38d2f0
unknown <i200825@nu.edu.pk>
2025-06-03 02:47:26 +0500

    Update next-themes to resolve peer dependency conflict

--- nextjs-base/next.config.ts
+++ nextjs-base/next.config.ts
@@ -1,17 +1,26 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.freepik.com/**",
-      },
-    ],
-  },
-  // if used turbopack
-  // transpilePackages: ["next-mdx-remote"],
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "img.freepik.com/**",
+//       },
+//     ],
+//   },
+//   // if used turbopack
+//   // transpilePackages: ["next-mdx-remote"],
+// };
 
-export default nextConfig;
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Only add basePath if your site should be served from a subdirectory
+  // basePath: '/your-subdirectory',
+  trailingSlash: true,
+  output: 'export' // Remove this if you need server-side features
+}
+
+module.exports = nextConfig
